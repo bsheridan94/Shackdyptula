c0b98b4fe05203a12417f97fe6ab2c0535a80ecc
Brian Sheridan <btsheridan94@gmail.com>
2017-06-09 02:41:43 +0100

    completed assignment 3 and 4

---
+++ assignment4/helloworld.c
@@ -0,0 +1,27 @@
+/*
+ * Assignment #1
+ * Hello Wolrd Module
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int hello_init(void)
+{
+	pr_debug("Hello World!\r\n");
+	return 0;
+}
+
+static void hello_exit(void)
+{
+	pr_debug("Hello World has been unloaded\r\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Brian Sheridan");
+MODULE_DESCRIPTION("SHACKDYPTULA ASSIGNMENT #1");
+
